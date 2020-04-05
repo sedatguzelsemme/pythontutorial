56c985501898b6e639436e0b5de392a2a249ead6
Sedat Güzelşemme <sedatguzelsemme@gmail.com>
2020-04-05 14:10:07 +0300

    set and list files created

---
+++ src/List/list.py
@@ -0,0 +1,6 @@
+# we are  
+lst = [1,2,3,4,5,6,7,8,9]
+
+ls = list()
+# list is a keyword so we can not use list as a variable
+print(type(ls))
---
+++ src/Set/set.py
@@ -0,0 +1,3 @@
+# set is a keyword so we can not use set as a variable
+_set = set()
+print(type(_set))
